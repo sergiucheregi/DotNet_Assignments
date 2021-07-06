a82e31ff9fd3f00ae784ff68317d1fcd9adcdad4
Sergiu Cheregi <sergiu.cheregi@accesa.eu>
2021-07-06 17:26:31 +0300

    First assignment

---
+++ Assignment_001/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Assignment_001
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Asignment:
+            // Write a program in C# to find the element with the maximum and minimum length in an array of strings. 
+            string[] collection = new String[] { "Cupcake", "Cake", "Candy", "Coffeee", "Tobleroneeee" };
+
+            int minLength = collection[0].Length;
+            int maxLength = collection[0].Length;
+            string minLengthString = string.Empty;
+            string maxLengthString = string.Empty;
+
+            for (int i = 1; i < collection.Length; i++)
+            {
+                if(collection[i].Length < minLength)
+                {
+                    minLength = collection[i].Length;
+                    minLengthString = collection[i];
+                }
+
+                if(collection[i].Length > maxLength)
+                {
+                    maxLength = collection[i].Length;
+                    maxLengthString = collection[i];
+                }
+            }
+
+            Console.WriteLine("The smallest string is {0} and has a length of: {1}", minLengthString, minLength);
+            Console.WriteLine("The largest string is {0} and has a length of: {1}", maxLengthString, maxLength);
+        }
+    }
+}
---
+++ Assignment_002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_002
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello d!");
+        }
+    }
+}
